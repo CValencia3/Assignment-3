1a3212cfa29a3e64dae7dc383e70e2077a9a7520
CValencia3 <valen193@mail.chapman.edu>
2019-03-27 05:06:14 +0000

    Added test file

---
+++ testFile.cpp
@@ -0,0 +1,39 @@
+//Taken from Game of Life assignment
+#ifndef GAME_RULES
+
+#include "game_rules.h"
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+// Returns whether the cell should live, die, or stay the same
+char GameRules::Evaluate(int neighbors, char current){
+    // Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+    if(neighbors < 2)(
+        return '-';
+    // Any live cell with two or three live neighbors lives on to the next generation.
+    else if(neighbors == 2)
+        return current;
+    // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+    else if(neighbors == 3)
+        return 'X';
+    // Any live cell with more than three live neighbors dies, as if by overpopulation.
+    else if(neighbors > 3)
+        return '-';
+}
+
+bool GameRules::CheckSimilarities(char*** a1, char*** a2, int ySize, int xSize){
+    //For every x and y position
+    for (int y = 0; y < ySize; ++y)
+        for (int x = 0; x < xSize; ++x)
+            //Check if they are the same
+            if((*a1)[y][x] != (*a2)[y][x])
+                return false;
+
+    return true;
+}
+
+#endif
